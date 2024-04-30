087f0f5f4219c8b63bb04ba224898c423a4386b0
IanQuan <quanian031229@gmail.com>
2024-04-30 04:34:56 -0400

    updated react dom

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyle';
 import Typography from './styles/Typography';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <>
     <GlobalStyles />
     <Typography />
     <App />
-  </>,
-  document.getElementById('root')
+  </>
 );
